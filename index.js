17bf84baff8a2326c4fac0f0ecee21e6394f8caf
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-09 23:07:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function scuberGreetingForFeet(rideLength) {
 
   if(rideLength <= 400){
     return "This one is on me!"
+  } else if (rideLength > 2000 ) {
+    return "I will gladly take your thirty bucks."
   }
 }
 
