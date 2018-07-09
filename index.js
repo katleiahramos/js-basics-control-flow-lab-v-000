061fa56ddc42817c655829dea0a3de2b6c434f8d
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-09 23:02:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Write your code in this file!
 function scuberGreetingForFeet(rideLength){
-  
+
   if rideLength <= 400{
     return "This one is on me!"
   }
 }
 
-console.log(ride)
-
+console.log(scuberGreetingForFeet(40))
