fe96a17a4c693575ffef67fab7a81df5156dace8
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-09 23:18:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ function ternaryCheckCity(city){
   }
 }
 
-function switchOnCharmFromTip(){
-  
+function switchOnCharmFromTip(tip){
+  if (tip === "generous"){
+    return "Thank you so much."
+  } else if (tip === "not as generous"){
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
 }
